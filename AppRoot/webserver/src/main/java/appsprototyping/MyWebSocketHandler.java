4bedf9b3bdae22177a186b2b57e6a5f1333f465d
Maciej Aleksandrowicz <macvek@gmail.com>
2022-11-12 20:30:15 +0100

    preset for websocket

---
+++ AppRoot/webserver/src/main/java/appsprototyping/MyWebSocketHandler.java
@@ -0,0 +1,14 @@
+package appsprototyping;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.WebSocketMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+@Component
+public class MyWebSocketHandler extends TextWebSocketHandler {
+    @Override
+    public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
+        super.handleMessage(session, message);
+    }
+}
--- AppRoot/webserver/src/main/java/appsprototyping/controllers/Welcome.java
+++ AppRoot/webserver/src/main/java/appsprototyping/controllers/Welcome.java
@@ -1,16 +1,15 @@
 package appsprototyping.controllers;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @Slf4j
 public class Welcome {
 
-    @GetMapping("/welcome")
+    @GetMapping("/")
     public String welcome() {
-        log.info("WELCOME");
-        return "Welcome";
+        return "index.html";
     }
 }
