d49407f88a3b91bfbd962cf77ddc03608b568674
Maciej Aleksandrowicz <macvek@gmail.com>
2023-02-03 14:30:07 +0100

    added remote root dir noted

--- Jenkins/jenkins-agent.sh
+++ Jenkins/jenkins-agent.sh
@@ -2,4 +2,5 @@
 #docker run -i --rm --network devnet --name jenkins-agent01 --init jenkins/agent java -jar /usr/share/jenkins/agent.jar
 
 # vv from jenkins itself
+# + it must be registered with /home/jenkins/agent
 docker run -i --rm --network devnet --name jenkins-agent01 --init jenkins/agent java -jar /usr/share/jenkins/agent.jar -jnlpUrl http://jenkins:8080/manage/computer/jenkins%2Dagent01/jenkins-agent.jnlp -secret XXXX965637f86148d643b494b194a59c929c1f4ca6b531d95f376b57b57bb54d -workDir /home/jenkins/agent
